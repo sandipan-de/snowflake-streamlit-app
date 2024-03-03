a9adcb084fe0de39bbf76c49ac987fcc4363218c
Sandipan De <47730546+sandipan-de@users.noreply.github.com>
2024-03-03 13:51:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
-session = get_active_session()
+session = snowflake.snowpark.context.get_active_session()
 my_dataframe = session.table('smoothies.public.fruit_options')\
         .select(col('FRUIT_NAME'))
 ingradient_list = \
