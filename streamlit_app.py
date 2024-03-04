6e3fe2526340af02449a32a811084e4d598c8eff
Sandipan De <47730546+sandipan-de@users.noreply.github.com>
2024-03-03 16:04:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table('smoothies.public.fruit_options')\
         .select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingradient_list = \
         st.multiselect(
@@ -34,6 +34,10 @@ if ingradient_list:
     ingradient_string = ''
     for fruit_chosen in ingradient_list:
         ingradient_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen , ' is ', search_on,'.')
+        
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
